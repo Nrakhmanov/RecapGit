cd486ef88e16e4337518e5b4046b4a27084cda71
Nuriddin Rakhmanov <nuriddin.rakhmanov@gmail.com>
2021-06-30 17:51:28 -0600

    new commit 3

--- src/test/java/Hello123.java
+++ src/test/java/Hello123.java
@@ -2,5 +2,6 @@ public class Hello123 {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Bye");
+        System.out.println("bye bye");
     }
 }
