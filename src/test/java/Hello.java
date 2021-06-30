64882acd881ecadbe97b24f3dbcd9226bc53bc0a
Nuriddin Rakhmanov <nuriddin.rakhmanov@gmail.com>
2021-06-30 17:32:32 -0600

    more review commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("Recapping github");
+        System.out.println("Recapping github, boring");
     }
 }
